b40bbf3596f02d7575941c95aed6cea80017019e
panddr <panddr@gmail.com>
2022-06-27 19:19:26 +0300

    wip

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ window.addEventListener("load", (event) => {
   const timeline = document.querySelector(".timeline");
   const timelineButton = document.querySelector(".timeline-button");
   const colRight = document.querySelector(".col-right");
+  const iframes = document.querySelectorAll(".iframe-resizer");
   let lastKnownScrollPosition = 0;
   let ticking = false;
 
@@ -28,6 +29,7 @@ window.addEventListener("load", (event) => {
 
   setTimelineFixed(window.scrollY);
   window.addEventListener("resize", function (e) {
+    [...iframes].map((iframe) => resizeIframe(iframe));
     setTimelineFixed(window.scrollY);
     if (window.innerWidth >= 1024) {
       body.classList.remove("disable-touch-scroll");
@@ -48,4 +50,25 @@ window.addEventListener("load", (event) => {
       ticking = true;
     }
   });
+
+  const resizeIframe = function (iframe) {
+    const iframeWrapper = iframe.querySelector("div");
+    const iframeContent = iframeWrapper.querySelector("iframe");
+    const defaultHeight = iframeContent.getAttribute("height");
+    const defaultWidth = iframeContent.getAttribute("width");
+    const isBandcamp = iframeContent.getAttribute("src").search("bandcamp") > 1;
+    let height;
+
+    if (
+      defaultWidth &&
+      defaultHeight &&
+      !defaultWidth.match(/\%/) &&
+      !isBandcamp
+    ) {
+      const ratio = parseFloat(defaultWidth) / parseFloat(defaultHeight);
+      height = iframeWrapper.clientWidth / ratio;
+      iframeContent.style.height = height + "px";
+    }
+  };
+  [...iframes].map((iframe) => resizeIframe(iframe));
 });

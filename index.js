f1b91d6522bccab29fa247d775c4b76b77db2136
panddr <panddr@gmail.com>
2022-06-27 13:18:18 +0300

    design

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 window.addEventListener("load", (event) => {
+  const body = document.querySelector("body");
   const timeline = document.querySelector(".timeline");
-  const nav = document.querySelector("nav");
-  const main = document.querySelector("main");
+  const timelineButton = document.querySelector(".timeline-button");
   const colRight = document.querySelector(".col-right");
   let lastKnownScrollPosition = 0;
   let ticking = false;
-  main.style.paddingTop = nav.offsetHeight;
 
-  setTimelineFixed(window.scrollY);
   function setTimelineFixed(scrollPos) {
-    if (scrollPos >= colRight.offsetTop) {
+    if (scrollPos >= colRight.offsetTop && window.innerWidth >= 1024) {
       timeline.classList.add("fixed");
     } else {
       timeline.classList.remove("fixed");
     }
   }
 
+  timelineButton.addEventListener("click", function (e) {
+    if (timeline.classList.contains("active")) {
+      body.classList.remove("disable-touch-scroll");
+      timeline.classList.remove("active");
+      timelineButton.classList.remove("active");
+    } else {
+      body.classList.add("disable-touch-scroll");
+      timeline.classList.add("active");
+      timelineButton.classList.add("active");
+    }
+  });
+
+  setTimelineFixed(window.scrollY);
   document.addEventListener("resize", function (e) {
     setTimelineFixed(window.scrollY);
   });
